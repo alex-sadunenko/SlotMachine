00f42b6d6691ecb23960ada62ac28617ddddffd3
alexandr_sadunenko <a.sadunenko@gmail.com>
2023-01-30 21:34:29 +0200

    Final

---
+++ Slot Machine/Helpers/Extansions.swift
@@ -0,0 +1,25 @@
+//
+//  Extansions.swift
+//  Slot Machine
+//
+//  Created by Alex on 22.01.2023.
+//
+
+import SwiftUI
+
+extension Text {
+    func scoreLabelStyle() -> Text {
+        self
+            .foregroundColor(Color.white)
+            .font(.system(size: 10, weight: .bold, design: .rounded ))
+    }
+    
+    func scoreNumberStyle() -> Text {
+        self
+            .foregroundColor(.white)
+            .font(.system(.title, design: .rounded))
+            .fontWeight(.heavy)
+    }
+}
+
+
---
+++ Slot Machine/Views/InfoView.swift
@@ -0,0 +1,73 @@
+//
+//  InfoView.swift
+//  Slot Machine
+//
+//  Created by Alex on 25.01.2023.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            LogoView()
+            
+            Spacer()
+            
+            Form {
+                Section(header: Text("About the application")) {
+                    FormRowView(firstItem: "Aplication", secondItem: "Slot Machine")
+                    FormRowView(firstItem: "Platforms", secondItem: "iPhone, iPad, Mac")
+                    FormRowView(firstItem: "Developer", secondItem: "John / Jane")
+                    FormRowView(firstItem: "Designer", secondItem: "Robert Petras")
+                    FormRowView(firstItem: "Music", secondItem: "Dan Lebowitz")
+                    FormRowView(firstItem: "Website", secondItem: "swiftuimasterclass.com")
+                    FormRowView(firstItem: "Copyright", secondItem: "2020 All rights reserved")
+                    FormRowView(firstItem: "Version", secondItem: "1.0.0")
+                }
+            }
+            .font(.system(.body, design: .rounded))
+        }
+        .padding(.top, 40)
+        .overlay(alignment: .topTrailing, content: {
+            Button {
+                audioPlayer?.stop()
+                self.presentationMode.wrappedValue.dismiss()
+            } label: {
+                Image(systemName: "xmark.circle")
+                    .font(.title)
+            }
+            .padding(.top, 30)
+            .padding(.trailing, 20)
+            .accentColor(.secondary)
+            .onAppear {
+                playSound(sound: "background-music", type: "mp3")
+            }
+        })
+    }
+}
+
+struct FormRowView: View {
+    
+    var firstItem: String
+    var secondItem: String
+    
+    var body: some View {
+        HStack {
+            Text(firstItem)
+                .foregroundColor(Color.gray)
+            Spacer()
+            Text(secondItem)
+        }
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
+
